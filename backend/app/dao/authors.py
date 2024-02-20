f8d3d02a9512d45e666ad11bbbed542ea96c399b
zhengzongwei <zhengzongwei@foxmail.com>
2024-02-20 19:34:41 +0800

    fe(authorDao):封装authorDao层

--- backend/app/dao/authors.py
+++ backend/app/dao/authors.py
@@ -7,19 +7,37 @@ from app.extensions import db
 from app.exceptions.authors import AuthorNotFound, AuthorDeletionError, AuthorCreationError, AuthorAlreadyExists
 
 
+def get_author_by_name(name):
+    return Author.query.filter_by(name=name, is_deleted=False).first()
+
+
 class AuthorDao(object):
 
     @staticmethod
-    def create_author(authors):
-        pass
+    def get_author_by_id(id):
+        return Author.query.filter_by(id=id, is_deleted=False).first()
 
     @staticmethod
-    def delete_author(author_ids):
-        pass
+    def get_author_by_name(name):
+        return Author.query.filter_by(name=name, is_deleted=False).first()
 
+    def create_author(self, author):
+        _author = self.get_author_by_name(author.name)
+        if _author:
+            raise AuthorAlreadyExists(author_id=_author.id)
+        new_author = Author(name=author.name)
+        db.session.add(new_author)
+        db.session.commit()
+        return new_author
+
+    def create_authors(self, authors):
+        for author in authors:
+            self.create_author(author)
+
+    def delete_author(self, author_ids):
         try:
             for author_id in author_ids:
-                author = Author.query.filter_by(id=author_id).first()
+                author = self.get_author_by_id(author_id)
                 if author or author.books:
                     # 检查是否有关联的书籍，如果有关联的书籍，不允许删除
                     raise AuthorDeletionError(author_id=author_id)
